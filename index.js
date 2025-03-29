9642cf696655db7d401a8057e19a6f0f7e72a2d9
rafael loiola dos santos <manga_loiola@hotmail.com>
2025-03-29 00:22:10 -0300

    aula functiont

--- index.js
+++ index.js
@@ -22,8 +22,37 @@ function main(){
 	const altura = 1.83;
 
 	const imc = calcularImc(peso, altura);
-	console.log ('IMC:'+imc.toFixed(2))
+	console.log ('1)IMC:'+imc.toFixed(2))
 	console.log (classificarImc(imc));
 }
 
 main();
+
+function calcularImc (peso, altura) {
+	return peso / Math.pow(altura, 2);
+}
+
+function classificarImc (imc){
+
+	if (imc < 18.5){
+		return  'Condição:abaixo do peso';
+	} else if (imc >= 18.5 && imc < 25 ){
+		return  'Condição:Peso Normal';
+	}else if (imc >=25 && imc < 30 ){
+		return 	'Condição:acima do Peso';
+	}else if (imc >= 30 && imc < 40){
+		return	 'Obeso';
+	} else {
+		return  'Condição:Obesidade Grave';
+	}
+}
+
+// Main
+(function (){
+	const peso = 150;
+	const altura = 1.83;
+
+	const imc = calcularImc(peso, altura);
+	console.log ('2)IMC:'+imc.toFixed(2))
+	console.log (classificarImc(imc));
+})();
